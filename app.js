d925a3e1b1f656632019b143d7cb600c67e75f2d
SakuOrdrTab <leinosensaku@gmail.com>
2025-11-25 20:27:06 +0200

    p11e12 done?

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+// health check for CI/CD pipe
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// // automatic health check error
+// app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw('error...  ')
+//   res.send('ok')
+// })
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
